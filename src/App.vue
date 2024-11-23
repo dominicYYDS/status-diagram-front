03914c4f03a167ef90d24040fbbf4ab6121f52e6
stzhangjk <stzhangjk@163.com>
2024-11-23 09:09:14 +0800

    init commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar title="状态图生成器"></v-app-bar>
+    <v-navigation-drawer>
+      <v-list-item link title="状态图Diagram配置"></v-list-item>
+      <v-list-item link title="状态Status配置"></v-list-item>
+      <v-list-item link title="位置Location配置"></v-list-item>
+      <v-list-item link title="位置值StatusValue配置"></v-list-item>
+    </v-navigation-drawer>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      Main Content
+    </v-main>
+  </v-app>
+
+</template>
